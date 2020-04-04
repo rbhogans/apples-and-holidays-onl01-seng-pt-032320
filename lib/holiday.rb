8c2375a495518ca8f790089740d2558195483cbf
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-04 06:01:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,6 +45,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
+end
 # code here
   # remember to return the updated hash
 
