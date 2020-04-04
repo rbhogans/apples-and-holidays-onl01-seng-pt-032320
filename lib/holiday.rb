d9795d30723791bfb2c173f6dede811d0c8c127e
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-04 06:13:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,3 +58,27 @@ def all_winter_holiday_supplies(holiday_hash)
   end
   array.flatten
 end
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ") << ":"
+      puts "  " + holiday + " " + supplies
+    end
+  end
+end
+
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, items|
+      if items.include?("BBQ")
+        holiday
+      end
+    end
+  end.flatten.compact
+end
